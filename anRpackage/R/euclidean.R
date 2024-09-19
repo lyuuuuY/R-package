1a02042024a884e87a9d64aaf5bf288b29844c55
lyuuuuY <lyuyanjie15@gmail.com>
2024-09-19 15:42:50 +0200

    second

--- anRpackage/R/euclidean.R
+++ anRpackage/R/euclidean.R
@@ -1,13 +1,29 @@
-#' Title
+#' Title euclidean
 #'
 #' @param x 
 #' @param y 
 #'
-#' @return
+#' @return r_2
 #' @export
 #'
-#' @examples
-euclidean <-
-function(x,y){
-  
+#' @examples euclidean(123612,13892347912)
+euclidean <- function(x,y){
+  if(x>=y){
+    r_1 <- x
+    r_2 <- y
+  }
+  else{
+    r_1 <- y
+    r_2 <- x
+  }
+  repeat{
+    q <- r_1%/%r_2
+    r_0 <- r_1%%r_2
+    if(r_0==0){
+      break
+    }
+    r_1 <- r_2
+    r_2 <- r_0
+  }
+  return(r_2)
 }
